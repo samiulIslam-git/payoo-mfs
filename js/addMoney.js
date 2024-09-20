5c352f6e9b7fb8e1be0ec79e1096ed9e69dca021
samiulIslam-git <kh.sami2017@gmail.com>
2024-09-20 16:54:12 +0600

    added new js file and updated homeHtml file

---
+++ js/addMoney.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-add-money')
+.addEventListener('click',function(event){
+    event.preventDefault();
+
+    const addMoney = document.getElementById('input-add-money').value;
+
+    const pinNumber = document.getElementById('input-pin-number').value;
+    console.log(pinNumber, addMoney);
+
+})
